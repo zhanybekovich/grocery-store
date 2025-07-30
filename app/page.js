7a023180d6e919c39db3492988f9096c615d71f4
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-30 19:11:51 +0600

    install shadcn

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Button>Button</Button>
     </div>
   );
 }
